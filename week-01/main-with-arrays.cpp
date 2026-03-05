39b1bf9670b32b558172685e07f09296a1c24aa3
Maya Lekova <mslekova@chromium.org>
2026-03-05 11:31:14 +0200

    Fix whitespace on older files

--- week-01/main-with-arrays.cpp
+++ week-01/main-with-arrays.cpp
@@ -4,45 +4,45 @@ using std::cin, std::cout;
 
 // Заделя масив в динамичната памет (на heap-а)
 double* allocateArray(size_t length) {
-    double* result = new double[length];
-    // да проверим дали е успешно заделяне
-    return result;
+  double* result = new double[length];
+  // да проверим дали е успешно заделяне
+  return result;
 }
 
 // Прочита `length` числа от конзолата и ги записва
 // в динамичен масив, в out_array
 void readArray(size_t length, double* out_array) {
-    double d;
-    for (int i = 0; i < length; ++i) {
-        cin >> d;
-        // тук можем да валидираме координатите, например
-        // дали са в определен интервал: [-1000, 1000] да речем
-        out_array[i] = d;
-    }
+  double d;
+  for (int i = 0; i < length; ++i) {
+    cin >> d;
+    // тук можем да валидираме координатите, например
+    // дали са в определен интервал: [-1000, 1000] да речем
+    out_array[i] = d;
+  }
 }
 
 // Извежда динамично заделен масив с дължина `length` на конзолата
 void printArray(size_t length, double* dyn_array) {
-    for (int i = 0; i < length; ++i) {
-        cout << dyn_array[i] << " ";
-    }
-    cout << std::endl;
+  for (int i = 0; i < length; ++i) {
+    cout << dyn_array[i] << " ";
+  }
+  cout << std::endl;
 }
 
 int main() {
-    size_t points_count = 0;
-    cin >> points_count;
+  size_t points_count = 0;
+  cin >> points_count;
 
-    double* x_coords = allocateArray(points_count);
-    double* y_coords = allocateArray(points_count);
+  double* x_coords = allocateArray(points_count);
+  double* y_coords = allocateArray(points_count);
 
-    readArray(points_count, x_coords);
-    readArray(points_count, y_coords);
+  readArray(points_count, x_coords);
+  readArray(points_count, y_coords);
 
-    cout << "X coords: " << std::endl;
-    printArray(points_count, x_coords);
-    cout << "Y coords: " << std::endl;
-    printArray(points_count, y_coords);
+  cout << "X coords: " << std::endl;
+  printArray(points_count, x_coords);
+  cout << "Y coords: " << std::endl;
+  printArray(points_count, y_coords);
 
-    return 0;
+  return 0;
 }
--- week-01/main.cpp
+++ week-01/main.cpp
@@ -3,44 +3,44 @@
 using std::cin, std::cout;
 
 struct Point {
-    double x;
-    double y;
+  double x;
+  double y;
 };
 
 // Заделя масив на heap-а
 Point* allocatePointArray(size_t length) {
-    Point* result = new Point[length];
-    return result;
+  Point* result = new Point[length];
+  return result;
 }
 
 void readPointArray(size_t length, Point* out_array) {
-    Point p;
-    for (int i = 0; i < length; ++i) {
-        cin >> p.x >> p.y;
-        out_array[i] = p;
-    }
+  Point p;
+  for (int i = 0; i < length; ++i) {
+    cin >> p.x >> p.y;
+    out_array[i] = p;
+  }
 }
 
 void printPointArray(size_t length, Point* dyn_array) {
-    for (int i = 0; i < length; ++i) {
-        cout << "(" << dyn_array[i].x
-             << ", " << dyn_array[i].y << ") ";
-    }
-    cout << std::endl;
+  for (int i = 0; i < length; ++i) {
+    cout << "(" << dyn_array[i].x
+      << ", " << dyn_array[i].y << ") ";
+  }
+  cout << std::endl;
 }
 
 int main() {
-    size_t points_count = 0;
-    cin >> points_count;
+  size_t points_count = 0;
+  cin >> points_count;
 
-    Point* points = allocatePointArray(points_count);
+  Point* points = allocatePointArray(points_count);
 
-    readPointArray(points_count, points);
+  readPointArray(points_count, points);
 
-    cout << "X, Y coords: " << std::endl;
-    printPointArray(points_count, points);
-    // TODO: да имплементираме сканирането на Греъм (Graham scan)
-    // за изчисляване на изпъкналата обвивка на точките в `points`.
+  cout << "X, Y coords: " << std::endl;
+  printPointArray(points_count, points);
+  // TODO: да имплементираме сканирането на Греъм (Graham scan)
+  // за изчисляване на изпъкналата обвивка на точките в `points`.
 
-    return 0;
+  return 0;
 }
