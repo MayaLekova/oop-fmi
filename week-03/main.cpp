f0e7b355a301f9eaac11793be6ce365b1459ad86
Maya Lekova <mslekova@chromium.org>
2026-03-05 14:36:41 +0200

    Add implementation from the seminar

--- week-03/main.cpp
+++ week-03/main.cpp
@@ -8,6 +8,69 @@
   ползваме за отбелязване на невалидни интервали.
 */
 
+// глобална променлива
+int my_var = 42;
+
+class Interval {
+ public:
+  Interval();
+  Interval(int a, int b);
+
+  void init(int a, int b);
+  bool isValid() const {
+    return lower <= upper;
+  }
+
+  Interval merge(const Interval& other);
+
+  static Interval INVALID_INTERVAL;
+  static int counter;
+
+ private:
+  int lower;
+  int upper;
+};
+
+// инициализиране на статичните член-данни на класа
+Interval Interval::INVALID_INTERVAL = Interval();
+int Interval::counter = 0;
+
+Interval::Interval() {
+  lower = 0;
+  upper = 0;
+}
+
+Interval::Interval(int a, int b) {
+  init(a, b);
+}
+
+void Interval::init(int a, int b) {
+  if (a <= b) {
+    lower = a;
+    upper = b;
+  } else {
+    lower = b;
+    upper = a;
+  }
+}
+
+Interval Interval::merge(const Interval& other) {
+  // this->lower === lower
+  int lowest = (lower < other.lower ? lower : other.lower);
+  int highest = (upper > other.upper ? upper : other.upper);
+
+  return Interval(lowest, highest);
+}
+
+// пример за друг клас, ComplexNumber с разделени декларация и дефиниция
+// class ComplexNumber {
+//   void Interval();
+// };
+
+// void ComplexNumber::Interval() {
+//    ..
+// }
+
 /*
 Да реализираме следните операции към него:
 
@@ -29,6 +92,11 @@
   валидността му, като го използваме за цели и дробни числа.
 */
 
+// дефинициите на член-функциите на класа Interval, когато
+// започнем да го правим шаблонен:
+// template <typename T>
+// Interval<T> Interval<T>::merge(const Interval<T>& other) {...}
+
 /*
 3. Да реализираме структура Date, която съдържа следните данни:
   
@@ -38,7 +106,20 @@
   невалидна, връща false, ако съвпадат - също.
 */
 
+struct Date {
+  // TODO
+};
+
+bool operator<(Date a, Date b) {
+  // TODO
+  return false;
+}
+
 /*
-4. Да демоснтрираме използването на класа Interval с току-що създадената
+4. Да демонстрираме използването на класа Interval с току-що създадената
   структура за дата.
 */
+
+int main() {
+  return 0;
+}
